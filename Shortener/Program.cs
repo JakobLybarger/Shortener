2785372a77f5d1ff517c6575b3608206720189f2
Jakob Lybarger <jakoblybarger911@gmail.com>
2024-06-27 23:52:24 -0500

    Add caching and make redirect count update a background process

--- Shortener/Program.cs
+++ Shortener/Program.cs
@@ -1,7 +1,10 @@
+using System.Threading.Channels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Shorten.Data;
 using Shorten.Data.Data;
+using Shortener;
 using Shortener.Models;
 using Shortener.Utils;
 
@@ -9,6 +12,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Shorten");
 builder.Services.AddDbContext<ShortenContext>(options => options.UseSqlServer(connectionString));
+
+// Add Cache
+builder.Services.AddMemoryCache();
+
+// Add Channel
+builder.Services.AddSingleton(Channel.CreateUnbounded<string>());
+
+// Add Background service
+builder.Services.AddHostedService<RedirectUpdater>();
 
 var app = builder.Build();
 
@@ -18,6 +30,7 @@ var mappings = new Dictionary<string, string>();
 app.MapPost("/shorten", async (
     [FromBody] ShortenRequest request,
     ShortenContext dbContext,
+    IMemoryCache cache,
     CancellationToken cancellationToken) =>
 {
     var shortenedUrl = string.Empty;
@@ -47,22 +60,39 @@ app.MapPost("/shorten", async (
         RedirectCount = 0
     };
 
+    cache.Set(shortenedUrl, request.Url, TimeSpan.FromMinutes(5));
+
     await dbContext.AddAsync(urlItem, cancellationToken);
     await dbContext.SaveChangesAsync(cancellationToken);
 
     return Results.Ok(new UrlResult { Url = $"{url}/{shortenedUrl}" });
 });
 
-app.MapGet("/{shortenedUrl}", async (string shortenedUrl, ShortenContext dbContext) =>
+app.MapGet("/{shortenedUrl}", async (
+    string shortenedUrl,
+    ShortenContext dbContext,
+    IMemoryCache cache,
+    Channel<string> channel,
+    CancellationToken cancellationToken) =>
 {
-    var exists = await dbContext.Shorten.FirstOrDefaultAsync(urlItem => urlItem.Id == shortenedUrl);
-    if (exists is null) return Results.NotFound("There is no mapping for this URL");
+    var longUrl = string.Empty;
+    if (cache.TryGetValue(shortenedUrl, out string? originalUrl))
+    {
+        longUrl = originalUrl ?? string.Empty;
+    }
+    else
+    {
+        var exists = await dbContext.Shorten.FirstOrDefaultAsync(urlItem => urlItem.Id == shortenedUrl, cancellationToken);
+        if (exists is null) return Results.NotFound("There is no mapping for this URL");
+        longUrl = exists.MappedUrl;
 
-    exists.RedirectCount++;
+        cache.Set(shortenedUrl, longUrl, TimeSpan.FromMinutes(5));
+    }
 
-    await dbContext.SaveChangesAsync();
+    var writer = channel.Writer;
+    await writer.WriteAsync(shortenedUrl, cancellationToken);
 
-    return Results.Redirect(exists.MappedUrl);
+    return Results.Redirect(longUrl);
 });
 
 app.Run();
---
+++ Shortener/RedirectUpdater.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+using Microsoft.EntityFrameworkCore;
+using Shorten.Data.Data;
+
+namespace Shortener;
+
+public class RedirectUpdater : BackgroundService
+{
+    private readonly Channel<string> _channel;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public RedirectUpdater(IServiceScopeFactory serviceScopeFactory, Channel<string> channel)
+    {
+        _channel = channel;
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var reader = _channel.Reader;
+
+        await foreach (var id in reader.ReadAllAsync(cancellationToken))
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ShortenContext>();
+
+            await dbContext.Shorten
+                .Where(item => item.Id == id)
+                .ExecuteUpdateAsync(setters => setters.SetProperty(item => item.RedirectCount, item => item.RedirectCount + 1), cancellationToken);
+        }
+    }
+}
